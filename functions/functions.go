b3d1e6487d81b6d8c8ef2b0f7bb0784187b00ff5
Nandan Satheesh <nandan.199776@gmail.com>
2019-08-03 22:11:59 +0530

    Functions in Go 2

--- functions/functions.go
+++ functions/functions.go
@@ -6,6 +6,28 @@ func main() {
 	fmt.Println(fact(12))
 	fmt.Println(fact(5))
 	fmt.Println(add(10, 30, 30))
+	a, b := 10, 30
+	a, b = swap(a, b)
+	fmt.Println(a, b)
+
+	array := []int{1, 2, 3, 4, 5}
+	fmt.Println(printArrayElements(array...))
+
+}
+
+func printArrayElements(elements ...int) int {
+	sum := 0
+	for _, i := range elements {
+		sum += i
+		fmt.Println(i)
+
+	}
+	fmt.Println(sum)
+	return sum
+}
+
+func swap(a, b int) (int, int) {
+	return b, a
 }
 
 func add(a, b, c int) int {

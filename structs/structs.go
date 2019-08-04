681ba431b30b4bccb848fe70d561082c3ba7c270
Nandan Satheesh <nandan.199776@gmail.com>
2019-08-04 12:02:00 +0530

    Slay with Interfaces

--- structs/structs.go
+++ structs/structs.go
@@ -2,9 +2,33 @@ package main
 
 import "fmt"
 
+type EmptyInterface interface {
+	displaySomething() string
+}
+
+type Calculation interface {
+	calculateArea() float32
+	calculatePerimeter() float32
+}
+
 type Rectange struct {
 	length float32
 	bredth float32
+}
+
+type Empty struct {
+}
+
+type Circle struct {
+	radius float32
+}
+
+func (e Empty) displaySomething() string {
+	return "Print Something"
+}
+
+func (c *Circle) calculateArea() float32 {
+	return (22 / 7.0) * c.radius * c.radius
 }
 
 func (r *Rectange) calculateArea() float32 {
@@ -17,7 +41,13 @@ func (r *Rectange) calculatePerimeter() float32 {
 
 func main() {
 	shapeOne := Rectange{10, 20}
+	circleOne := Circle{10}
+	arrayOfInterfaces := []EmptyInterface{Empty{}, Empty{}}
+	for _, array := range arrayOfInterfaces {
+		fmt.Println(array.displaySomething())
+	}
 	fmt.Println(shapeOne)
 	fmt.Println(shapeOne.calculateArea())
 	fmt.Println(shapeOne.calculatePerimeter())
+	fmt.Println(circleOne.calculateArea())
 }

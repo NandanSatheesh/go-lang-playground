40469baf59bdb932c7cf61dacd7ab1872a4dd4c8
Nandan Satheesh <nandan.199776@gmail.com>
2019-08-07 20:38:59 +0530

    Added Solution 1 - Ex 1

--- gophercises/ex1/main.go
+++ gophercises/ex1/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var Questions []string
+var Answers []int64
+
+func init() {
+
+	file, err := os.Open("problem.csv")
+	checkErr(err)
+
+	csvData := csv.NewReader(file)
+
+	for {
+		row, err := csvData.Read()
+
+		if err == io.EOF {
+			break
+		}
+		Questions = append(Questions, row[0])
+		number, _ := strconv.ParseInt(row[1], 10, 64)
+		Answers = append(Answers, number)
+	}
+}
 
 func main() {
-	fmt.Println("123")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	score := 0
+	for i := 0; i < len(Questions); i++ {
+
+		fmt.Println("What's ", Questions[i], "?")
+
+		inputText, err := reader.ReadString('\n')
+		inputText = strings.Replace(inputText, "\n", "", -1)
+		checkErr(err)
+
+		userAnswer, err := strconv.ParseInt(inputText, 10, 64)
+		checkErr(err)
+
+		if userAnswer == Answers[i] {
+			score++
+		}
+
+	}
+
+	fmt.Println("Your score is ", score, ".")
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }

80497bbd19953797922f3d487d3097435af562a8
Nandan Satheesh <nandan.199776@gmail.com>
2019-08-08 18:04:16 +0530

    Errors in Go

---
+++ errors/errors.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func throwError(input int) (int, error) {
+
+	if input != 23 {
+		return 0, errors.New("This is wrong. How can you do this?")
+	}
+	return 0, nil
+}
+
+func main() {
+	_, err := throwError(2)
+	fmt.Println(err)
+}
